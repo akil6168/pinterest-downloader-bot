4070984bb9e597be8ee418913dba3c6c5b44c35b
akil6168 <141494894+akil6168@users.noreply.github.com>
2026-07-18 02:13:21 +0600

    Create downloader.py

---
+++ downloader.py
@@ -0,0 +1,19 @@
+import yt_dlp
+import os
+
+DOWNLOAD_DIR = "downloads"
+os.makedirs(DOWNLOAD_DIR, exist_ok=True)
+
+def download_media(url):
+    ydl_opts = {
+        'outtmpl': f'{DOWNLOAD_DIR}/%(id)s.%(ext)s',
+        'format': 'best',
+        'quiet': True,
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=True)
+        filepath = ydl.prepare_filename(info)
+
+    if filepath.endswith(('.jpg', '.jpeg', '.png', '.webp')):
+        return filepath, "photo"
+    return filepath, "video"
